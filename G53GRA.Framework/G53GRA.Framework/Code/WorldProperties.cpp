58678fa846cb0fbd76dbb251f9e11fde9f312456
BenRamsden <benramsdensii@gmail.com>
2017-05-08 09:19:41 +0100

    Updated  keypresses to use state==0 and state==1 after looking in the engine, key response a lot more fluid and simoultaneous now

--- G53GRA.Framework/G53GRA.Framework/Code/WorldProperties.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/WorldProperties.cpp
@@ -22,37 +22,32 @@ void WorldProperties::Update(const double& deltaTime)
 	globalPosition[1] += deltaTime * updateGlobalPosition[1];
 	globalPosition[2] += deltaTime * updateGlobalPosition[2];
 
-	updateGlobalPosition[0] = 0.0f;
-	updateGlobalPosition[1] = 0.0f;
-	updateGlobalPosition[2] = 0.0f;
-
-
 	globalVelocity[0] += deltaTime * updateGlobalVelocity[0];
 	globalVelocity[1] += deltaTime * updateGlobalVelocity[1];
 	globalVelocity[2] += deltaTime * updateGlobalVelocity[2];
-
-	updateGlobalVelocity[0] = 0.0f;
-	updateGlobalVelocity[1] = 0.0f;
-	updateGlobalVelocity[2] = 0.0f;
 
 }
 
 void WorldProperties::HandleSpecialKey(int key, int state, int x, int y)
 {
 	if (key == GLUT_KEY_UP) {
-		updateGlobalVelocity[2] = 0.4f;
+		if(state == 1) updateGlobalVelocity[2] = 0.4f;
+		if(state == 0) updateGlobalVelocity[2] = 0.0f;
 	}
 
 	if (key == GLUT_KEY_DOWN) {
-		updateGlobalVelocity[2] = -0.6f;
+		if (state == 1) updateGlobalVelocity[2] = -0.6f;
+		if (state == 0) updateGlobalVelocity[2] = 0.0f;
 	}
 
 	if (key == GLUT_KEY_RIGHT) {
-		updateGlobalPosition[0] = -globalVelocity[2] * 600;
+		if (state == 1) updateGlobalPosition[0] = -sqrt(abs(globalVelocity[2])) * 100;
+		if (state == 0) updateGlobalPosition[0] = 0.0f;
 	}
 
 	if (key == GLUT_KEY_LEFT) {
-		updateGlobalPosition[0] = globalVelocity[2] * 600;
+		if (state == 1) updateGlobalPosition[0] = sqrt(abs(globalVelocity[2])) * 100;
+		if (state == 0) updateGlobalPosition[0] = 0.0f;
 	}
 
 }

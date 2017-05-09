88496e9d3741fdabf5fddcd21cb3c308f1ce88c7
BenRamsden <benramsdensii@gmail.com>
2017-05-10 00:00:39 +0100

    Flipped flag texture to be right way around Added normal to flag for proper lighting

--- G53GRA.Framework/G53GRA.Framework/Code/Flag.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/Flag.cpp
@@ -46,13 +46,14 @@ void Flag::DrawFlag() {
 
 	glBegin(GL_QUADS);
 
-	glTexCoord2f(0, 0);
-	glVertex3f(0, 0, 0);
-	glTexCoord2f(0, 1);
-	glVertex3f(0, 1, 0);
-	glTexCoord2f(1, 1);
-	glVertex3f(1, 1, 0);
+	glNormal3f(0, 0, -1);
 	glTexCoord2f(1, 0);
+	glVertex3f(0, 0, 0);
+	glTexCoord2f(1, 1);
+	glVertex3f(0, 1, 0);
+	glTexCoord2f(0, 1);
+	glVertex3f(1, 1, 0);
+	glTexCoord2f(0, 0);
 	glVertex3f(1, 0, 0);
 
 	glEnd();
